b7ab035dd881794f84b6b28affe31e927144096e
Ole Umlauft <umlauft@bitside.de>
2023-10-13 19:15:58 +0200

    Add config .env

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import email
 import requests
 import base64
 
+from dotenv import dotenv_values
+
 from aiosmtpd.controller import Controller
 from aiosmtpd.smtp import SMTP as SMTPServer
 from aiosmtpd.smtp import Envelope as SMTPEnvelope
@@ -12,6 +14,7 @@ from aiosmtpd.smtp import Session as SMTPSession
 from email.message import Message
 
 
+global config
 
 class PushHandler:
   async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
@@ -47,11 +50,12 @@ class PushHandler:
     return '250 Message accepted for delivery'
 
 async def main(loop):
-  cont = Controller(PushHandler(), hostname='127.0.0.1', port=1025)
+  cont = Controller(PushHandler(), hostname=config["HOST"], port=1025)
   cont.start()
 
 
 if __name__ == '__main__':
+  config = dotenv_values("config/.env")
   logging.basicConfig(level=logging.DEBUG)
   loop = asyncio.get_event_loop()
   loop.create_task(main(loop=loop))
